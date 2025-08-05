08795f06528c9d46e75fe937cbc151f05c6c292a
ill Ryu <ryuill1@gmail.com>
2025-08-05 15:13:16 -0700

    add a test case to run abaqus

--- extensions/eig_fem/eig_fem_dis/__init__.py
+++ extensions/eig_fem/eig_fem_dis/__init__.py
@@ -5,3 +5,4 @@ from .remote_stress.cal_remote_stress import CalRemoteStress
 from .calforce.calforce_with_surface import CalForce
 from .mobility.mobility_with_surface import MobilityLaw
 from .surface_topology.surface_topology import Surface_Topology
+from .abaqus.run_abaqus import run_abaqus
