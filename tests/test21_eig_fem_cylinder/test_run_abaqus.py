9dfe0c24c026d9ccf16b48d1de1166bf6b5cc922
kyeongmi <kyeongmxi@snu.ac.kr>
2025-08-06 07:52:27 +0900

    add test case to run abaqus (donegit status)

--- tests/test21_eig_fem_cylinder/test_run_abaqus.py
+++ tests/test21_eig_fem_cylinder/test_run_abaqus.py
@@ -1,3 +1,5 @@
+''' This python script can run abaqus inp file with user subroutine which are included in subfolder ABAQUS
+'''
 import numpy as np
 import sys, os
 
@@ -15,3 +17,16 @@ from eig_fem_dis import CalForce as CalForce_withSurface, MobilityLaw as Mobilit
 
 #from eig_fem_dis.abaqus import run_abaqus
 from eig_fem_dis import run_abaqus
+
+config = {
+        "jobname_head": '',
+        "ABAQUS_input_filename": 'elas_axidisp',
+        "num_cpus": 1,
+        "umatname": 'elastic.f',
+        "foldername_ABAQUS": 'ABAQUS',
+
+        # "cmd_env": 'setenv.bat', 
+
+    }
+
+run_abaqus(config)
